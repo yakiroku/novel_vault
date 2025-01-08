13b3514c5e93809b15e32442a07efbe258347cfb
yakiroku <yakiroku@gmail.com>
2025-01-08 16:40:45 +0900

    bag

--- search/novel_search_factory.py
+++ search/novel_search_factory.py
@@ -1,7 +1,7 @@
 from search.nocturne_ranked_search import NocturneRankedSearch
 from search.nocturne_tag_search import NocturneTagSearch
 from search.nocturne_tag_weekly_search import NocturneTagWeeklySearch
-from search.pixiv_tag_search import PixivTagSearch
+# from search.pixiv_tag_search import PixivTagSearch
 from shared.enums.search_target import SearchTarget
 
 
@@ -25,7 +25,7 @@ class NovelSearchFactory:
                 return NocturneTagSearch()
             case SearchTarget.NOCTURNE_WEEKLY_TAG:
                 return NocturneTagWeeklySearch()
-            case SearchTarget.PIXIV_TAG:
-                return PixivTagSearch()
+            # case SearchTarget.PIXIV_TAG:
+            #     return PixivTagSearch()
             case _:
                 raise ValueError(f"Unsupported search target: {target}")
